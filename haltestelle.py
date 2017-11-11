648282ea4cbe93208b74399f2df5594e02ee6104
Auss Abbood <a.abbood@live.de>
2017-11-11 10:35:04 +0100

    zweiter param mit stop id

--- haltestelle.py
+++ haltestelle.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 URL = 'http://app.vrr.de/msapp/XML_DM_REQUEST?'
 
@@ -31,4 +32,35 @@ parameter = {
 'locationServerActive':'1',
 }
 
-r = requests.post(URL, parameter)
+step_2 = {
+'changeSpeed':'normal',
+'name_dm': '24043415',
+'type_dm': 'any',
+'useRealtime': '1',
+'coordListOutputFormat':'STRING',
+'canChangeMOT' :'0',
+'sessionID':'0',
+'itOptionsActive':'1',
+'coordOutputFormatTail':'5',
+'mode':'direct',
+# 'includeCompleteStopSeq':'0',
+'useAllStops':'0',
+# 'imparedOptionsActive':'1',
+'maxTimeLoop':'2',
+# 'useProxFootSearch':'1',
+'stateless':'1',
+'outputFormat':'JSON',
+'excludedMeans':'checkbox',
+'mergeDep':'1',
+'lineRestriction':'400',
+'depType':'stopEvents',
+'ptOptionsActive':'1',
+# 'trlTMOTvalue100':'0',
+'coordOutputFormat':'WGS84[DD.DDDDD]',
+'locationServerActive':'1',
+}
+
+
+
+r = requests.post(URL, step_2)
+pprint(r.json())
